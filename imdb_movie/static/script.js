d598cc36472cf9be1bb24afaf68dc9d6af99a3f9
Thuxinhdep2905 <thuna.22it@vku.udn.vn>
2025-05-13 10:02:52 +0700

    directory structure

--- imdb_movie/static/script.js
+++ imdb_movie/static/script.js
@@ -11,3 +11,13 @@
         }, false);
     });
 })();
+
+function deleteNote(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
+
