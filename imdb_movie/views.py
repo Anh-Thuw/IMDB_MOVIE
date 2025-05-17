f1c43b11e117fb78d8213267278304274e2ba3dd
Thuxinhdep2905 <thuna.22it@vku.udn.vn>
2025-05-18 01:32:14 +0700

    router

--- imdb_movie/views.py
+++ imdb_movie/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify 
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for 
 from tensorflow.keras.models import load_model
 import numpy as np
 import imdb_movie.forms as forms
@@ -13,35 +13,16 @@ import joblib
 
 views = Blueprint('views', __name__)
 
+@views.route('/', methods=['GET'])
+def index():
+    return render_template("login.html", user=current_user)
 
-@views.route('/', methods=['GET', 'POST'])
+@views.route('/home', methods=['GET'])
 @login_required
 def home():
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
-
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
-            db.session.commit()
-            flash('Note added!', category='success')
-
     return render_template("home.html", user=current_user)
 
 
-@views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
 
 #--------------------------------------------model prediction--------------------------------------------
 
